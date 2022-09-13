f85d87f9ed77e36fe4e7cc4af14d1431ea8d42b5
Betelhem <elbetelmisgan49@gmail.com>
2022-09-13 16:16:57 -0700

    task 0

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - prints the alphabet, in lowercase,
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 'a'; i <= 'z'; i++)
+		_putchar(i);
+	_putchar('\n');
+}
