1bad17454eaa401bc2219e5ada38c7479906f285
Betelhem <elbetelmisgan49@gmail.com>
2022-09-13 17:39:57 -0700

    task 06

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _abs - pirntsints theabsolute functio of the numebr
+ * @n: the number to be evvaluated
+ * Return: the abslolute value of the number
+ */
+int _abs(int)
+{
+	int n;
+
+	if (n > 0)
+		return (n);
+	else
+		return (-n);
+}
