edcb11a672dc4cdf304624d178eca6de222569fe
Betelhem <elbetelmisgan49@gmail.com>
2022-09-15 12:39:49 -0700

    task 09

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -13,11 +13,11 @@ void more_numbers(void)
 
 	for (a = 0; a < 10; a++)
 	{
-		for (b = 0; b < 15; b++)
+		for (b = 0; b <= 14; b++)
 		{
 			if (b > 9)
 			{
-				putchar((b % 10) + '0')
+				putchar((b / 10) + '0');
 			}
 			putchar(10);
 		}
