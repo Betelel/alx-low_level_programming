a3e39f3e1e06dacbcb2e75998ec783d77f03bfba
Betelhem <elbetelmisgan49@gmail.com>
2022-09-15 11:18:28 -0700

    task 00

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,15 +1,10 @@
-##ifndef MAIN_H
-#define MAIN_H
-
+#ifndef HEADER_FILE
+#define HEADER_FILE
 int _isupper(int c);
-int _isdigit(int c);
-int mul(int a, int b);
+int _isdigit(int);
+int mul(int, int);
 void print_numbers(void);
+int _putchar(char);
 void print_most_numbers(void);
 void more_numbers(void);
-void print_line(int n);
-void print_diagonal(int n);
-void print_square(int size);
-void print_triangle(int size);
-
-#endif /* MAIN_H */
+#endif
