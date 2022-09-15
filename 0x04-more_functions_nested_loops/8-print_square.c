2fc222be2bad09e7686a29ad2eec7459cfe00235
Betelhem <elbetelmisgan49@gmail.com>
2022-09-15 13:22:05 -0700

    uuu

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_square - prints a square, followed by a new line
+ * @size: parameter
+ * Return: returns nothing
+ */
+
+void print_square(int size)
+{
+	int in1, in2;
+
+	if (size > 0)
+	{
+		for (in1 = 0; in1 < size; in1++)
+		{
+			for (in2 = 0; in2 < (size - 1); in2++)
+			{
+				putchar('#');
+			}
+			
+			putchar('#');
+			putchar('\n');
+		}
+	}
+	else
+	{
+		putchar('\n');
+	}
+}
