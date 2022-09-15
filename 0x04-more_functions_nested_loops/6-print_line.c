c77bb99f877012af136fe8f662fd53df02fad32b
Betelhem <elbetelmisgan49@gmail.com>
2022-09-15 13:04:24 -0700

    task 0009

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -9,25 +9,9 @@
 
 void print_line(int n)
 {
-	int len, space;
-
-	if (n > 0)
+	while (n-- > 0)
 	{
-		for (len = 0; len < n; len++)
-		{
-			for (space = 0; space < len; space++)
-			{
-				putchar(' ');
-			}
-
-			putchar('\\');
-
-			if (len == (n - 1))
-			{
-				continue;
-			}
-			putchar('\n');
-		}
+		putchar('_');
 	}
 	putchar('\n');
 }
