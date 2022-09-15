0d48d15b1a1da9fe65a0bf574f014ae90af92485
Betelhem <elbetelmisgan49@gmail.com>
2022-09-15 10:55:33 -0700

    task 00

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * _isupper - checks for uppercase character
@@ -13,7 +12,6 @@ int _isupper(int c)
 	{
 		return (1);
 	}
-	else
-	{
-		return (0);
-	}
+
+	return (0);
+}
