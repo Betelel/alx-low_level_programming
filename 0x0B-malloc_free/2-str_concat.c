55520ca6855674fbc3745cfc550c64127e8bbc07
Betelhem <elbetelmisgan49@gmail.com>
2022-10-04 09:37:28 -0700

    task 02 mallock free

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,23 +1,41 @@
 #include "main.h"
-#include <stdio.h>
 #include <stdlib.h>
 
 /**
- * main - check the code for ALX School students.
- *
- * Return: Always 0.
- */
-int main(void)
+* str_concat - function that concatenates two strings.
+* @s1: - char1
+* @s2: - char2
+* Return: - char
+*/
+char *str_concat(char *s1, char *s2)
 {
-    char *s;
+	unsigned int i, j, x, y, z;
+	char *p;
 
-    s = _strdup("ALX SE");
-    if (s == NULL)
-    {
-        printf("failed to allocate memory\n");
-        return (1);
-    }
-    printf("%s\n", s);
-    free(s);
-    return (0);
+	if (s1 == NULL)
+		s1 = "";
+
+	if (s2 == NULL)
+		s2 = "";
+
+	for (i = 0; s1[i] != '\0'; i++)
+		;
+
+	for (j = 0; s2[j] != '\0'; j++)
+		;
+
+	p = malloc((i + j) * sizeof(char) + 1);
+
+	if (p == NULL)
+		return (NULL);
+
+	for (x = 0; x < i; x++)
+		p[x] = s1[x];
+
+	for (y = i, z = 0; y < i + j; y++, z++)
+		p[y] = s2[z];
+
+	p[i + j] = '\0';
+
+	return (p);
 }
